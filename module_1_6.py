afede6d0fcd3f3bea6191d12d3f708c282f6a8ff
SunnyBottom <kristinaeme@gmail.com>
2024-08-13 16:16:16 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Ilya': 1998, 'Nikita': 2004, 'Dasha': 2006}
+print(my_dict)
+print(my_dict['Ilya'])
+print(my_dict.get('Vladimir', 'такого человека не существует'))
+my_dict.update({'Sett': 2000,
+                'Xayah': 1900})
+print(my_dict)
+my_dict.pop('Nikita')
+print(my_dict)
+my_set = {1, 2, 4, 3 ,5 ,4 , 'apple', 'apple'}
+print(my_set)
+my_set.remove('apple')
+print(my_set)
